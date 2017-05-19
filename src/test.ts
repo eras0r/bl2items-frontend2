b9d522b44a119e13a351accddc36a5bcbb2b8350
eras0r <er4s0r@gmail.com>
2017-05-20 00:50:36 +0200

    configure karma tests to run with PhantomJS

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,11 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// polyfills required to run the tests within PhantomJS
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/string';
+import 'core-js/es6/array';
+
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy.js';
 import 'zone.js/dist/sync-test';
